235a681f703c94d660b4a51eb15149c3b6bd685a
Michael Poutre <m1kep.my.mail@gmail.com>
2023-08-21 20:31:05 -0700

    Tokenizer and parser completed

--- lib/actions/base.py
+++ lib/actions/base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Callable, Union
 
 
 class Action(ABC):
@@ -11,3 +12,19 @@ class Action(ABC):
     @abstractmethod
     def END_CHAR(self):
         pass
+
+    @classmethod
+    @abstractmethod
+    def parse_segment(
+        cls,
+        tokens: list[str],
+        start_chars: list[str],
+        end_chars: list[str],
+        parent_parser: Callable[[list[str]], Union[str, 'Action']],
+    ) -> 'Action':
+        pass
+
+    def depth_repr(self, depth=1):
+        raise NotImplementedError()
+
+PromptSegment = str | Action
