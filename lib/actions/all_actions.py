c28099021bd67a0d209929c6d4a9cd069526ac5d
Michael Poutre <m1kep.my.mail@gmail.com>
2023-08-24 23:51:40 -0700

    remove old actions

--- lib/actions/all_actions.py
+++
@@ -1,6 +0,0 @@
-from .arith import ArithAction
-from .nudge import NudgeAction
-
-ALL_ACTIONS = [NudgeAction, ArithAction]
-ALL_START_CHARS = [action.START_CHAR for action in ALL_ACTIONS]
-ALL_END_CHARS = [action.END_CHAR for action in ALL_ACTIONS]
