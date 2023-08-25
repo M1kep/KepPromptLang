f8064d31e7b28f1eda2cbaafbe7719531cb0513c
Michael Poutre <m1kep.my.mail@gmail.com>
2023-08-24 21:09:50 -0700

    My -> PromptLang

--- lib/fun_clip_stuff.py
+++ lib/fun_clip_stuff.py
@@ -19,7 +19,7 @@ def slerp(val, low, high):
     res = (torch.sin((1.0-val)*omega)/so).unsqueeze(1)*low + (torch.sin(val*omega)/so).unsqueeze(1) * high
     return res
 
-class MyCLIPTextEmbeddings(CLIPTextEmbeddings):
+class PromptLangCLIPTextEmbeddings(CLIPTextEmbeddings):
     def __init__(self, config: CLIPTextConfig):
         super().__init__(config)
 
@@ -85,10 +85,10 @@ class MyCLIPTextEmbeddings(CLIPTextEmbeddings):
         return embeddings
 
 
-class MyCLIPTextTransformer(CLIPTextTransformer):
+class PrompLangCLIPTextTransformer(CLIPTextTransformer):
     def __init__(self, config: CLIPTextConfig):
         super().__init__(config)
-        self.embeddings = MyCLIPTextEmbeddings(config)
+        self.embeddings = PromptLangCLIPTextEmbeddings(config)
 
     def forward(
             self,
@@ -175,10 +175,11 @@ class MyCLIPTextTransformer(CLIPTextTransformer):
         )
 
 
-class MyCLIPTextModel(CLIPTextModel):
+# This is necessary to pass the PromptLangCLIPTextTransformer
+class PromptLangTextModel(CLIPTextModel):
     def __init__(self, config: CLIPTextConfig):
         super().__init__(config)
-        self.text_model = MyCLIPTextTransformer(config)
+        self.text_model = PrompLangCLIPTextTransformer(config)
 
     def forward(
             self,
@@ -189,23 +190,6 @@ class MyCLIPTextModel(CLIPTextModel):
             output_hidden_states: Optional[bool] = None,
             return_dict: Optional[bool] = None,
     ) -> Union[Tuple, BaseModelOutputWithPooling]:
-        r"""
-        Returns:
-
-        Examples:
-
-        ```python
-        >>> from transformers import AutoTokenizer, CLIPTextModel
-
-        >>> model = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
-        >>> tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-
-        >>> inputs = tokenizer(["a photo of a cat", "a photo of a dog"], padding=True, return_tensors="pt")
-
-        >>> outputs = model(**inputs)
-        >>> last_hidden_state = outputs.last_hidden_state
-        >>> pooled_output = outputs.pooler_output  # pooled (EOS token) states
-        ```"""
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         return self.text_model(
