6444e2890a8fbe05af41168bce5012ac2ffca8f4
Michael Poutre <m1kep.my.mail@gmail.com>
2023-09-04 23:35:24 -0700

    feat(func/setDims): Add func

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,6 +5,7 @@ from custom_nodes.KepPromptLang.lib.actions.neg import NegAction
 from custom_nodes.KepPromptLang.lib.actions.norm import NormAction
 from custom_nodes.KepPromptLang.lib.actions.rand import RandAction
 from custom_nodes.KepPromptLang.lib.actions.scale_dims import ScaleDims
+from custom_nodes.KepPromptLang.lib.actions.set_dims import SetDims
 from custom_nodes.KepPromptLang.lib.actions.slerp import SlerpAction
 from custom_nodes.KepPromptLang.lib.actions.sum import SumAction
 from custom_nodes.KepPromptLang.lib.parser.registration import register_action
@@ -18,3 +19,4 @@ register_action(SumAction)
 register_action(SlerpAction)
 register_action(AverageAction)
 register_action(ScaleDims)
+register_action(SetDims)
