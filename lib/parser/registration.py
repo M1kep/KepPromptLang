e59aaa499dd6b62c5d4a94bc16a19f2c21639c77
Michael Poutre <m1kep.my.mail@gmail.com>
2023-09-04 18:00:35 -0700

    fix(action reg): Don't allow multiple actions with same name

--- lib/parser/registration.py
+++ lib/parser/registration.py
@@ -5,6 +5,12 @@ from custom_nodes.KepPromptLang.lib.action.base import Action
 action_registry: Dict[str, Type[Action]] = {}
 
 def register_action(action: Type[Action]) -> None:
+    """
+
+    :rtype: object
+    """
+    if action.name in action_registry:
+        raise ValueError(f"Action {action.name} already registered")
     action_registry[str(action.name)] = action
 
 def get_action_by_name(name: str) -> Type[Action]:
